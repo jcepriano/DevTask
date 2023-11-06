53c973603f0e8b21d4ee4df340abd8c7cde1b0b5
jcepriano <jamescepriano@gmail.com>
2023-11-06 15:11:01 -0700

    Add project files.

---
+++ DevTask/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DevTask.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
