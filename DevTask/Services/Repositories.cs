ce51019df2a5862ed8df4f959c11e07bc39b165b
jcepriano <jamescepriano@gmail.com>
2023-11-07 11:06:49 -0700

    Set Up project and now working on calling the api

---
+++ DevTask/Services/Repositories.cs
@@ -0,0 +1,31 @@
+﻿using DevTask.Models;
+using System.Text.Json;
+
+namespace DevTask.Services
+{
+    public class Repositories
+    {
+        private readonly HttpClient _httpClient;
+
+        public Repositories(IHttpClientFactory clientFactory)
+        {
+            _httpClient = clientFactory.CreateClient("GitHubApi");
+        }
+
+        public async Task<GitHubRepository> GetRepository()
+        {
+            var url = string.Format("/jcepriano/SceneSherpa");
+            var result = new GitHubRepository();
+            var response = await _httpClient.GetAsync(url);
+
+            var stringResponse = await response.Content.ReadAsStringAsync();
+            result = JsonSerializer.Deserialize<GitHubRepository>(stringResponse,
+                new JsonSerializerOptions()
+                {
+                    PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+                });
+
+            return result;
+        }
+    }
+}
---
+++ DevTaskTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace DevTaskTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
