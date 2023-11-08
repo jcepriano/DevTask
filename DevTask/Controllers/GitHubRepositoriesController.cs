da0e368bfb169db1726d277abb4b3fdd3648dea9
jcepriano <jamescepriano@gmail.com>
2023-11-08 14:35:50 -0700

    app is not running

--- DevTask/Controllers/GitHubRepositoriesController.cs
+++ DevTask/Controllers/GitHubRepositoriesController.cs
@@ -12,23 +12,33 @@ namespace DevTask.Controllers
             _repositories = repositories;
         }
 
+        /*
         public async Task<IActionResult> Index(string owner, string repoName)
         {
             var repo = new List<GitHubRepository>();
             repo = await _repositories.GetRepositories(owner, repoName);
             return View(repo);
         }
+        */
 
-        public IActionResult Create()
+        public IActionResult CreateRepo()
         {
             return View();
         }
 
         [HttpPost]
-        public async Task<IActionResult> Index(string owner, User user)
+        public async Task<IActionResult> Index(GitHubRepository repo)
         {
-            owner = user.GitHubUsername;
-            return RedirectToAction("show", "UsersController");
+            var result = await _repositories.GetRepositories(repo.Name);
+
+            if (result.Any())
+            {
+                return RedirectToAction("show", "UsersController");
+            }
+            else
+            {
+                return BadRequest("Error occurred while creating the repo");
+            }
         }
     }
 }
--- DevTask/Services/Repositories.cs
+++ DevTask/Services/Repositories.cs
@@ -7,16 +7,20 @@ namespace DevTask.Services
     public class Repositories
     {
         private readonly HttpClient _httpClient;
+        private readonly User _user;
 
-        public Repositories(IHttpClientFactory clientFactory)
+        public Repositories(IHttpClientFactory clientFactory, User user)
         {
             _httpClient = clientFactory.CreateClient("GitHubApi");
             _httpClient.DefaultRequestHeaders.UserAgent.Add(
                 new ProductInfoHeaderValue("YourAppName", "1.0"));
+
+            _user = user;
         }
 
-        public async Task<List<GitHubRepository>> GetRepositories(string owner, string repoName)
+        public async Task<List<GitHubRepository>> GetRepositories(string repoName)
         {
+            string owner = _user.GitHubUsername;
             var url = $"repos/{owner}/{repoName}";
             var result = new List<GitHubRepository>();
 
