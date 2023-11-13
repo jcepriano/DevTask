9368593ed93cec7c15e9561f30755175761b1474
jcepriano <jamescepriano@gmail.com>
2023-11-13 10:37:49 -0700

    not working

--- DevTask/Controllers/TasksController.cs
+++ DevTask/Controllers/TasksController.cs
@@ -1,12 +1,47 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DevTask.DataAccess;
+using DevTask.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace DevTask.Controllers
 {
     public class TasksController : Controller
     {
+        private readonly DevTaskContext _context;
+
+        public TasksController(DevTaskContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [Route("/users/{userId:int}/repos/{repoId:int}/tasks/new")]
+        public IActionResult New(int userId, int repoId)
+        {
+            var repo = _context.Users
+                .Where(u => u.Id == userId)
+                .Include(u => u.GitHubRepositories)
+                .ThenInclude(repo => repo.Tasks)
+                .FirstOrDefault();
+
+            return View(repo);
+        }
+
+        [HttpPost]
+        [Route("/users/{userId:int}/repos/{repoId:int}/tasks")]
+        public IActionResult Create(int userId)
+        {
+            var repo = _context.Users
+                .Where(u => u.Id == userId)
+                .Include(u => u.GitHubRepositories)
+                .ThenInclude(repo => repo.Tasks)
+                .FirstOrDefault();
+
+
         }
     }
 }
